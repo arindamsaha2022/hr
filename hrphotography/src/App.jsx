613d96cd539a39bde8e6083a967014b82d31add1
arindam saha <112413589+arindamsaha2022@users.noreply.github.com>
2024-10-02 00:17:38 -0400

    appjsx

--- hrphotography/src/App.jsx
+++ hrphotography/src/App.jsx
@@ -1,7 +1,7 @@
 
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom"
 import Header from "./components/Header"
-import "./styles/App.scss";
+import "./styles/app.scss";
 import "./styles/Header.scss";
 import About from "./pages/About";
 import Home from "./pages/Home";
